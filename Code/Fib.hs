e79525fb4b3f2166aed0988e58a2c34e468f2dc6
Patricia Oliveira <up201905427@fe.up.pt>
2021-11-10 19:29:56 +0000

    Fix Fibbonacci funcations, try optimized version

--- Code/Fib.hs
+++ Code/Fib.hs
@@ -1,31 +1,38 @@
--- TODO :: melhorar mensagens de erro!
+-- Nésimo numero da sequencia corresponde ao valor de ordem n-1 (ordem inicia em 0)
 
 -- 1.1) Funcao Recursiva
 fibRec :: (Integral a) => a -> a
-fibRec 0 = 0
-fibRec 1 = 1
-fibRec 2 = 1 -- acaba por nao ser necessario
+fibRec 1 = 0
+fibRec 2 = 1
 fibRec n 
-    | n > 0     = fibRec (n-1) + fibRec (n-2) 
-    | otherwise = error "n invalido"
+    | n > 0     = fibRec (n - 2) + fibRec (n - 1) 
+    | otherwise = error "argumento não positivo"
 
 
 -- 1.2) Versao Otimizada
+somaUltimos2 :: Num a => [a] -> a
+somaUltimos2 xs = (last xs) + (last (init xs))
+
+expandirSeq :: Num a => [a] -> Int -> [a]
+expandirSeq xs 0 = xs
+expandirSeq xs n = expandirSeq (xs ++ [somaUltimos2 xs]) (n - 1)
+
 fibLista :: (Integral a) => a -> a
 fibLista n 
-    | n < 0                 = error "n invalido"
-    | i < (length lista)    = lista !! i
-    | otherwise             = fibLista (n-2) + fibLista (n-1)
+    | n <= 0    = error "argumento não positivo"
+    | i < len   = lista !! i
+    | otherwise = (expandirSeq lista (i + 1 - len)) !! i
     where lista = [0, 1, 1, 2, 3, 5, 8, 13]
-          i     = fromIntegral(n)
-
+          len   = length lista
+          i     = fromIntegral(n - 1)
+          
 
 -- 1.3) Versao com Lista Infinita
 fibListaInfinita :: (Integral a) => a -> a
 fibListaInfinita n 
-    | n < 0     = error "n invalido"
+    | n <= 0     = error "argumento não positivo"
     | otherwise = let fibs = (0 : 1 : [a + b | (a,b) <- zip fibs (tail fibs)])
-                  in fibs !! fromIntegral(n) -- Nota: é apenas n -> fib de 0 = 0, 0 conta como ordem
+                  in fibs !! fromIntegral(n - 1)
 
 
 -- NOTA:: aparece um exemplo da sequencia fibonacci nos exercicos do livro "Learning by solving problems"
