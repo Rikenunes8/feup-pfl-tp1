a18a75f1c89daaba7dd725aa5dfcc9a3349951a4
Henrique Nunes <hrn2001@gmail.com>
2021-11-11 18:05:23 +0000

    2.5 falta implementar o operador igual dos BigNumbers, pelo menos desta maneira

--- Code/BigNumber.hs
+++ Code/BigNumber.hs
@@ -10,6 +10,20 @@ instance Show BigNumber where
     show (Positive digits) = "Positive " ++ show digits
     show (Negative digits) = "Negative " ++ show digits
 
+-- Funções auxiliares
+zipDefault0::[Int] -> [Int] -> [(Int, Int)]
+zipDefault0 [] []         = []
+zipDefault0 (x:xs) []     = (x,0):(zipDefault0 xs [])
+zipDefault0 [] (y:ys)     = (0,y):(zipDefault0 [] ys)
+zipDefault0 (x:xs) (y:ys) = (x,y):(zipDefault0 xs ys)
+
+bnToList::BigNumber -> [Int]
+bnToList (Positive l) = l
+
+
+cleanLeft0s::BigNumber -> BigNumber
+cleanLeft0s (Positive l) = Positive (reverse (dropWhile (== 0) (reverse l)))
+
 
 -- 2.2) converte uma string em big-number
 stringToN :: String -> [Int]
@@ -33,26 +47,19 @@ output (Positive bn) = "+" ++ nToString bn
 
 
 -- 2.4) soma dois big-numbers.
-bnToList::BigNumber -> [Int]
-bnToList (Positive l) = l
-
-zipDefault0::[Int] -> [Int] -> [(Int, Int)]
-zipDefault0 [] []         = []
-zipDefault0 (x:xs) []     = (x,0):(zipDefault0 xs [])
-zipDefault0 [] (y:ys)     = (0,y):(zipDefault0 [] ys)
-zipDefault0 (x:xs) (y:ys) = (x,y):(zipDefault0 xs ys)
-
-cleanLeft0s::BigNumber -> BigNumber
-cleanLeft0s (Positive l) = Positive (reverse (dropWhile (== 0) (reverse l)))
-
 somaBN::BigNumber -> BigNumber -> BigNumber
 somaBN a b  = let aux = [x+y | (x,y) <- zipDefault0 (bnToList a) (bnToList b)]
               in cleanLeft0s (Positive [ x `mod` 10 + y `div` 10 | (x, y) <- zipDefault0 aux (0:aux)])
 
 
--- 2.5) subtraí dois big-numbers.
--- subBN :: BigNumber -> BigNumber -> BigNumber
-
+-- 2.5) subtrai dois big-numbers.
+subBN :: BigNumber -> BigNumber -> BigNumber
+-- é preciso implementar o operador < para funcionar
+--subBN a b | a >= b    = cleanLeft0s (Positive [ x `mod` 10 + y `div` 10 | (x, y) <- zipDefault0 (aux a b) (0:(aux a b))])
+--          | otherwise = cleanLeft0s (Positive [ x `mod` 10 + y `div` 10 | (x, y) <- zipDefault0 (aux b a) (0:(aux b a))]) -- Não sei se isto funciona
+--          where aux c d = [x-y | (x,y) <- zipDefault0 (bnToList c) (bnToList d)]
+subBN a b = let aux = [x-y | (x,y) <- zipDefault0 (bnToList a) (bnToList b)]
+            in Positive [ x `mod` 10 + y `div` 10 | (x, y) <- zipDefault0 aux (0:aux)]
 
 
 -- 2.6) multiplica dois big-numbers.
