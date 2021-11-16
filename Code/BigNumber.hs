55e973c93ad0751364225455596a127baf4e5062
Patricia Oliveira <up201905427@fe.up.pt>
2021-11-16 10:59:59 +0000

    apply recomended changes by prof

--- Code/BigNumber.hs
+++ Code/BigNumber.hs
@@ -56,40 +56,38 @@ output (Positive bn) = "+" ++ nToString bn
 
 
 -- 2.4) soma dois big-numbers.
--- TODO:: change name
-sumPar :: [(Int, Int)] -> [Int]
-sumPar [] = []
-sumPar ((a,b):[]) = (a + b) `mod` 10 : let r = (a + b) `div` 10 
+sumDigits :: [(Int, Int)] -> [Int]
+sumDigits [] = []
+sumDigits ((a,b):[]) = (a + b) `mod` 10 : let r = (a + b) `div` 10 
                                        in if (r /= 0) then [r] else []
-sumPar ((a,b):(c,d):l) = (a + b) `mod` 10 : sumPar ((c, d + ((a + b) `div` 10)) : l)
+sumDigits ((a,b):(c,d):l) = (a + b) `mod` 10 : sumDigits ((c, d + ((a + b) `div` 10)) : l)
 
 somaBN :: BigNumber -> BigNumber -> BigNumber
-somaBN (Negative a) (Negative b) = Negative (cleanLeft0s (reverse (sumPar (zip0 (reverse a) (reverse b)) ) ))
+somaBN (Negative a) (Negative b) = Negative (cleanLeft0s (reverse (sumDigits (zip0 (reverse a) (reverse b)) ) ))
 somaBN (Negative a) (Positive b) = subBN (Positive b) (Positive a)
 somaBN (Positive a) (Negative b) = subBN (Positive a) (Positive b)
-somaBN (Positive a) (Positive b) = Positive (cleanLeft0s (reverse (sumPar (zip0 (reverse a) (reverse b)) ) ))
+somaBN (Positive a) (Positive b) = Positive (cleanLeft0s (reverse (sumDigits (zip0 (reverse a) (reverse b)) ) ))
 
 
 -- 2.5) subtrai dois big-numbers.
--- TODO:: change name
-subPar :: [(Int, Int)] -> [Int]
-subPar [] = []
-subPar ((a,b):[]) = [(a - b)]
-subPar ((a,b):(c,d):l) 
-    | a >= b    = (a - b) : subPar ((c, d) : l)
-    | otherwise = (a + 10 - b) : subPar ((c, d + 1) : l)
+subDigits :: [(Int, Int)] -> [Int]
+subDigits [] = []
+subDigits ((a,b):[]) = [(a - b)]
+subDigits ((a,b):(c,d):l) 
+    | a >= b    = (a - b) : subDigits ((c, d) : l)
+    | otherwise = (a + 10 - b) : subDigits ((c, d + 1) : l)
 
 
 subBN :: BigNumber -> BigNumber -> BigNumber
 subBN (Negative a) (Negative b) = subBN  (Positive b) (Positive a)
 subBN (Negative a) (Positive b) = somaBN (Negative a) (Negative b)
 subBN (Positive a) (Negative b) = somaBN (Positive a) (Positive b)
-subBN (Positive a) (Positive b) | Positive a < Positive b = Negative (cleanLeft0s (reverse (subPar (zip0 (reverse b) (reverse a)) ) ))
-                                | otherwise               = Positive (cleanLeft0s (reverse (subPar (zip0 (reverse a) (reverse b)) ) ))
+subBN (Positive a) (Positive b) | Positive a < Positive b = Negative (cleanLeft0s (reverse (subDigits (zip0 (reverse b) (reverse a)) ) ))
+                                | otherwise               = Positive (cleanLeft0s (reverse (subDigits (zip0 (reverse a) (reverse b)) ) ))
 
 
 -- 2.6) multiplica dois big-numbers.
--- Multiplica um numero por um unico digito, levando o que fica como resto (?-TODO tentar arranjar mecanismo que nao precisa do r-?)
+-- Multiplica um numero por um unico digito, levando o que fica como resto
 mulDigit :: [Int] -> Int -> Int -> [Int]
 mulDigit _  0 _ = [0]
 mulDigit xs 1 _ = xs
@@ -103,7 +101,7 @@ mulParcelas xs ys = [(take i (repeat 0)) ++ (mulDigit xs y 0) | (y, i) <- zip ys
 
 -- TODO :: nao queria repetir 4 vezes a mesma coisa em baixo uma vez que so muda o sinal, mas tambem nao gosto muito assim...
 mulNs :: [Int] -> [Int] -> [Int]
-mulNs a b = cleanLeft0s (bnList (foldr somaBN (Positive []) [Positive (reverse p) | p <- mulParcelas (reverse a) (reverse b)]))
+mulNs a b = bnList (foldr somaBN (Positive []) [Positive (reverse p) | p <- mulParcelas (reverse a) (reverse b)])
 
 mulBN :: BigNumber -> BigNumber -> BigNumber
 mulBN (Negative a) (Negative b) = Positive (mulNs a b)
@@ -111,60 +109,14 @@ mulBN (Negative a) (Positive b) = Negative (mulNs a b)
 mulBN (Positive a) (Negative b) = Negative (mulNs a b)
 mulBN (Positive a) (Positive b) = Positive (mulNs a b)
 
--- QUESTAO.. QUAL É MAIS HASKELL?
-isNegative :: BigNumber -> Bool
-isNegative (Negative _) = True
-isNegative _            = False
-
-isPositive :: BigNumber -> Bool
-isPositive (Positive _) = True
-isPositive _            = False
-
-mulBN' :: BigNumber -> BigNumber -> BigNumber
-mulBN' a b 
-    | (isNegative a && isNegative b) || (isPositive a && isPositive b) = Positive m
-    | otherwise                                                        = Negative m
-    where m = cleanLeft0s (bnList (foldr somaBN (Positive []) [Positive (reverse p) | p <- mulParcelas (reverse (bnList a)) (reverse (bnList b))]))
-
-
 
 -- 2.7) efetua a divisão inteira de dois big-numbers. Retornar um par “(quociente, resto)” 
 -- [Assumindo que ambos os argumentos são positivos]
-
-
--- lastSmallBN :: BigNumber -> BigNumber -> (BigNumber, BigNumber)
--- lastSmallBN n d = last (takeWhile (\(q, dq) -> dq <= n) [ (i, mulBN d i) | i <- sucBN (Positive [0]) ])
 
 -- Produz uma sucessão infinita a partir de i
 sucBN :: BigNumber -> [BigNumber]
 sucBN i = i : sucBN (somaBN i (Positive [1]))
 
-divBN'' :: BigNumber -> BigNumber -> (BigNumber, BigNumber)
-divBN'' a b = (q, subBN a dq)
-    where (q, dq) = last (takeWhile (\(q, dq) -> dq <= a) [ (i, mulBN b i) | i <- sucBN (Positive [0]) ])
-
--- Produz lista infinita dos mútiplos de n
-multiplosBN :: BigNumber -> [BigNumber]
-multiplosBN n = [mulBN x i | (x, i) <- zip (repeat n) (sucBN (Positive [1]))]
-
--- Retorna o comprimento de uma lista em BigNumber
-lengthBN :: [a] -> BigNumber
-lengthBN [] = Positive [0]
-lengthBN (x:xs) = somaBN (Positive [1]) (lengthBN xs)
-
--- 4 / 5
 divBN :: BigNumber -> BigNumber -> (BigNumber, BigNumber)
-divBN (Positive a) (Positive b) = (q, r)
-          where x = takeWhile (< (Positive a)) (multiplosBN (Positive b))
-                q = lengthBN x
-                r = if (q == (Positive [0])) then (Positive a) else subBN (Positive a) (last x)
-                
--- como se assume que são Positivos é necessário colocar (Positive a) como parametro ou basta (a)?
-divBN' :: BigNumber -> BigNumber -> (BigNumber, BigNumber)
-divBN' a b = (lengthBN x, subBN a (last x))
-          where x = takeWhile (< a) (multiplosBN b)
-
---25 / 4
---[0, 0, 0, 0, 0, 24]
--- - > (24, 6) -> q = 6 ; r (25 - 24)
--- ESTRATEGIA: 
+divBN a b = (q, subBN a dq)
+    where (q, dq) = last (takeWhile (\(q, dq) -> dq <= a) [ (i, mulBN b i) | i <- sucBN (Positive [0]) ])
